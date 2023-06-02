2b836cdc03d89f8192c478fdac3826d60ce0614e
DA Mac 141 <damac141@DAs-MacBook-Air.local>
2023-06-02 11:55:46 +0200

    Added service files

---
+++ src/app/get-one/get-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-one',
+  templateUrl: './get-one.component.html',
+  styleUrls: ['./get-one.component.css']
+})
+export class GetOneComponent {
+
+}
