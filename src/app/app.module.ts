ee2d9b8146e602f5b35f13e5ae7ed6982813279c
MOzil-10 <mukosibudeli@gmail.com>
2023-06-05 00:19:39 +0200

    Added Categories and our top sellers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeContentComponent } from './home-content/home-content.component';
 import { CardComponent } from './card/card.component';
-=======
 import { GetOneComponent } from './get-one/get-one.component';
-import { AllSidePanelComponent } from './all-side-panel/all-side-panel.component';
 import { AllCardsComponent } from './all-cards/all-cards.component';
+import { AllSidePanelComponent } from './all-side-panel/all-side-panel.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
->>>>>>> 89df6c3f1a5aa7acd22485f79ce9c50128718b6c
-
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
+
     HomeContentComponent,
-    CardComponent
-=======
+    CardComponent,
     GetOneComponent,
     AllSidePanelComponent,
     AllCardsComponent,
     NavigationBarComponent
->>>>>>> 89df6c3f1a5aa7acd22485f79ce9c50128718b6c
   ],
   imports: [
     BrowserModule,
