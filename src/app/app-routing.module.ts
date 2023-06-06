f661d9736b9022adae555f65d435d69885ace78f
PortiaRavhutulu <ravhutuluportia@gmail.com>
2023-06-06 15:53:53 +0200

    fixed git errors on Dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,23 +4,16 @@ import { AllCardsComponent } from './all-cards/all-cards.component';
 import { GetOneComponent } from './get-one/get-one.component';
 
 import { AboutComponent } from './about/about.component';
-
-
-const routes: Routes = [{path : 'shop', component : AllCardsComponent},
-{path : 'viewProduct', component : GetOneComponent},
-{path : 'about', component : AboutComponent}
-
 import { HomeContentComponent } from './home-content/home-content.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 
-
-
 export const routes: Routes = [
   {path : 'shop', component : AllCardsComponent},
   {path : 'viewProduct', component : GetOneComponent},
-  {path : '', component: HomeContentComponent}
+  {path : '', component: HomeContentComponent},
+  {path : 'about', component : AboutComponent}
 
 ];
 
