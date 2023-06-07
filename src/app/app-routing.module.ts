5b5f4a3c40abe34014bf195414d2f6ccca73e617
Delphia Sekhukhune <sekhukhunedelphia@gmail.com>
2023-06-07 09:05:15 +0200

    Adding a new feature -the footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllCardsComponent } from './all-cards/all-cards.component';
 import { GetOneComponent } from './get-one/get-one.component';
-
 import { AboutComponent } from './about/about.component';
 
-
-const routes: Routes = [{path : 'shop', component : AllCardsComponent},
-{path : 'viewProduct', component : GetOneComponent},
-{path : 'about', component : AboutComponent}
 
 import { HomeContentComponent } from './home-content/home-content.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { FooterComponent } from './footer/footer.component';
 
 
 export const routes: Routes = [
   {path : 'shop', component : AllCardsComponent},
   {path : 'viewProduct', component : GetOneComponent},
-  {path : '', component: HomeContentComponent}
+  {path : '', component: HomeContentComponent},
+  {path : 'about', component : AboutComponent},
+
 
 ];
 
